d8a79e39119973b049530712ed61ac2531ee40a1
Varun S <varun.saxena101@gmail.com>
2018-11-27 00:30:37 -0500

    fixed visual page

--- js/main.js
+++ js/main.js
@@ -3,3 +3,21 @@ function searchButton() {
 
 }
 
+function displayLineGraph() {
+  var img = document.getElementById("graphImg");
+  img.src = "imgs/line-graph.png";
+  img.alt = "Line Graph";
+}
+
+function displayBarGraph() {
+  var img = document.getElementById("graphImg");
+  img.src = "imgs/bar-graph.png";
+  img.alt = "Bar Graph";
+}
+
+function displayCircleGraph() {
+  var img = document.getElementById("graphImg");
+  img.src = "imgs/circle-graph.png";
+  img.alt = "Circle Graph";
+}
+
