769f646e4aa0b622c7c1a673e880c44710cc7721
acsheffield <andrewcsheffield@gatech.edu>
2019-02-15 14:58:44 -0500

    Finished Directory

--- website/src/Directory.js
+++ website/src/Directory.js
@@ -4,34 +4,51 @@ import createDatabaseClient from "./Database";
 const databaseClient = createDatabaseClient();
 
 export default class Directory extends Component {
-	constructor(props) {
-		super(props);
-		// this.state.allTerms is empty for now until loaded in componentDidMount()
-		this.state = {
-			allTerms: []
-		}
-	}
+  constructor(props) {
+    super(props);
+    // this.state.allTerms is empty for now until loaded in componentDidMount()
+    this.state = {
+      allTerms: []
+    };
+  }
 
   componentDidMount() {
-  	// called once when you go to this page
-  	databaseClient.getAllTerms().then(docs => {
-  		// get all terms from databaseClient and then set this.state.allTerms to be the list of terms
+    // called once when you go to this page
+    databaseClient.getAllTerms().then(docs => {
+      // get all terms from databaseClient and then set this.state.allTerms to be the list of terms
       this.setState({
-      	allTerms: docs
+        allTerms: docs
       });
     });
   }
 
   render() {
-  	// renders every time this.state is updated
-  	console.log(this.state.allTerms); // see this in browser's console (Right click => Inspect element => Console)
+    // renders every time this.state is updated
+    console.log(this.state.allTerms); // see this in browser's console (Right click => Inspect element => Console)
     return (
-    	<div>
-    		<p> Hello World </p>
-    	</div>
-    	// stuff here TODO
-    	// render this.state.allTerms in a list
-    	// see https://reactjs.org/docs/lists-and-keys.html
-    	);
+      <div>
+        <this.TermList />
+      </div>
+      // stuff here TODO
+      // render this.state.allTerms in a list
+      // see https://reactjs.org/docs/lists-and-keys.html
+    );
   }
+
+  TermList = () => {
+    const terms = this.state.allTerms;
+    const listItems = terms.map(term => (
+      <li>
+        <a
+          href={"#"}
+          //onClick={e =>
+          //history.push("?" + queryString.stringify({v: term }));
+          //}
+        >
+          {term}
+        </a>
+      </li>
+    ));
+    return <ul>{listItems}</ul>;
+  };
 }
