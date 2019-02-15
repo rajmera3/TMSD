18b693caddc3a71e93a47ad39ea84f7b47214f93
luisgzz <lgpcollege@gmail.com>
2019-02-15 13:21:07 -0500

    Added a basic navigation syste,. Moved code in app.js to MainPage.js

--- website/src/App.js
+++ website/src/App.js
@@ -10,97 +10,26 @@ import Totals from "./Totals";
 import packageIcon from "./icons/icon-package.svg";
 import poweredBy from "./images/powered-by@2x.png";
 import TermPage from "./TermPage";
+import MainPage from "./MainPage";
 
-/*
 class App extends Component {
   render() {
     return (
       <Router>
-        <ScrollToTop>
-          <Route>
-            {({ location, history }) => (
-              <Search location={location} history={history}>
-                {({
-                  query,
-                  queryState,
-                  queryClass,
-                  searchActions,
-                  searchResults
-                }) => (
-                  <div>
-                    <div className="site-background" />
-                    <div className={`search-demo live-filtering ${queryClass}`}>
-                      <div className="search-demo__content">
-                        <div className="search-demo__header">
-                          <div className="search-demo__headings">
-                            <div className="search-demo__icon-wrap">
-                              <img
-                                src={packageIcon}
-                                alt="Dog Icon"
-                                className="search-demo__icon"
-                              />
-                            </div>
-                            <h1 className="search-demo__title">
-                              Time Machine Space Dinosaur
-                            </h1>
-                          </div>
-                          <div className="search-demo__input-wrapper">
-                            <input
-                              className="search-demo__text-input"
-                              placeholder="Search science fiction term or author&#8230;"
-                              value={query}
-                              onChange={e =>
-                                searchActions.updateQuery(e.target.value)
-                              }
-                            />
-                            <input
-                              type="submit"
-                              value="Search"
-                              className="button search-demo__submit"
-                            />
-                          </div>
-                        </div>
-
-                        <div className="search-demo__body">
-                          <div className="search-results">
-                            <div className="results">
-                              <div className="results__header">
-                                <Totals {...searchResults.pageState} />
-                              </div>
-                              <div className="results__body">
-                                <Results
-                                  results={searchResults.results}
-                                  queryState={queryState}
-                                  trackClick={searchActions.trackClick}
-                                />
-                              </div>
-                              <div className="results__footer">
-                                <Pagination
-                                  {...searchResults.pageState}
-                                  onPage={searchActions.updatePage}
-                                />
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                )}
-              </Search>
-            )}
-          </Route>
-        </ScrollToTop>
+        <div>
+          <Route path="/home" component={MainPage} />
+          <Route path="/term" component={TermPage} />
+        </div>
       </Router>
     );
   }
 }
-*/
 
+/*
 class App extends Component {
   render() {
     return <TermPage />;
   }
-}
+}*/
 
 export default App;
---
+++ website/src/MainPage.js
@@ -0,0 +1,129 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import ScrollToTop from "./ScrollToTop";
+import Pagination from "./Pagination";
+import Results from "./Results";
+import Search from "./Search";
+import Totals from "./Totals";
+
+import packageIcon from "./icons/icon-package.svg";
+import poweredBy from "./images/powered-by@2x.png";
+import TermPage from "./TermPage";
+
+class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { title: " " };
+  }
+
+  componentWillMount() {}
+
+  render() {
+    return (
+      <div>
+        <ScrollToTop>
+          <Route>
+            {({ location, history }) => (
+              <Search location={location} history={history}>
+                {({
+                  query,
+                  queryState,
+                  queryClass,
+                  searchActions,
+                  searchResults
+                }) => (
+                  <div>
+                    <div className="site-background" />
+                    <div className={`search-demo live-filtering ${queryClass}`}>
+                      <div className="search-demo__content">
+                        <div className="search-demo__header">
+                          <div className="search-demo__headings">
+                            <div className="search-demo__icon-wrap">
+                              <img
+                                src={packageIcon}
+                                alt="Dog Icon"
+                                className="search-demo__icon"
+                              />
+                            </div>
+                            <h1 className="search-demo__title">
+                              Time Machine Space Dinosaur
+                            </h1>
+                          </div>
+                          <div className="search-demo__input-wrapper">
+                            <input
+                              className="search-demo__text-input"
+                              placeholder="Search science fiction term or author&#8230;"
+                              value={query}
+                              onChange={e =>
+                                searchActions.updateQuery(e.target.value)
+                              }
+                            />
+                            <input
+                              type="submit"
+                              value="Search"
+                              className="button search-demo__submit"
+                            />
+                          </div>
+                        </div>
+
+                        <div className="search-demo__body">
+                          <div className="search-results">
+                            <div className="results">
+                              <div className="results__header">
+                                <Totals {...searchResults.pageState} />
+                              </div>
+                              <div className="results__body">
+                                <Results
+                                  results={searchResults.results}
+                                  queryState={queryState}
+                                  trackClick={searchActions.trackClick}
+                                />
+                              </div>
+                              <div className="results__footer">
+                                <Pagination
+                                  {...searchResults.pageState}
+                                  onPage={searchActions.updatePage}
+                                />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </Search>
+            )}
+          </Route>
+        </ScrollToTop>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  header: {
+    width: "75vw",
+    margin: "auto",
+    marginTop: "20px",
+    textAlign: "center"
+  },
+  headerText: {
+    textAlign: "center"
+  },
+  graph: {
+    width: "90vw",
+    height: "50vh",
+    margin: "auto",
+    marginTop: "20px"
+  },
+  definition: {
+    width: "60vw",
+    height: "30vh",
+    margin: "auto",
+    marginTop: "40px"
+  }
+};
+
+export default MainPage;
