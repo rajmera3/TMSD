9b8f57d518bde4377058f4776dbcd3e6ebfdb33d
luisgzz <lgpcollege@gmail.com>
2019-04-09 01:12:36 -0400

    Heroku setup

--- webscraper/setEnv.py
+++
@@ -1,8 +0,0 @@
-import os
-import json
-with open(os.path.expanduser("~/.bashrc"), "a") as outfile:
-    # 'a' stands for "append"  
-    d = ''
-    with open('./keys/service_account.json') as f:
-        d = json.dumps(json.load(f))
-    outfile.write("export GOOGLE_SERVICE_ACCOUNT=" + d)
